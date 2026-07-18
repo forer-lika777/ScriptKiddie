c86556d266ee97bfc586010539bd89eb9b94ed84
Zhang <Zhang747326@outlook.com>
2026-07-18 16:13:47 +0800

    添加反转布尔转换器

---
+++ ScriptKiddie.WinUI/Utils/Converters/InverseBoolConverter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptKiddie.WinUI.Utils.Converters;
+
+internal class InverseBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is bool boolean)
+        {
+            return !boolean;
+        }
+
+        return true;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        if (value is bool boolean)
+        {
+            return !boolean;
+        }
+
+        return true;
+    }
+}
