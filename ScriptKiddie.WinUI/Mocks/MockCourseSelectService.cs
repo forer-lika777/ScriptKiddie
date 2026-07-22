fb26289efa5c704dad5c2c5dc6b14f16acafd73a
Zhang <Zhang747326@outlook.com>
2026-07-22 20:49:03 +0800

    mock服务实现列表更新

--- ScriptKiddie.WinUI/Mocks/MockCourseSelectService.cs
+++ ScriptKiddie.WinUI/Mocks/MockCourseSelectService.cs
@@ -1,9 +1,10 @@
-﻿using ScriptKiddie.WinUI.Models;
+﻿using Microsoft.Extensions.Logging;
+using ScriptKiddie.WinUI.Models;
 using ScriptKiddie.WinUI.Services;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,18 +12,68 @@ namespace ScriptKiddie.WinUI.Mocks;
 
 public class MockCourseSelectService : ICourseSelectService
 {
+    private readonly HttpClientProvider httpClientProvider;
+    private readonly ILogger<MockCourseSelectService> logger;
+
+    private CourseResponse? selectableCourses = null;
+    private List<CourseItem>? selectedCourses = null;
+
+    public MockCourseSelectService(HttpClientProvider httpClientProvider, ILogger<MockCourseSelectService> logger)
+    {
+        this.httpClientProvider = httpClientProvider;
+        this.logger = logger;
+        SetSelectableCourses();
+        SetSelectedCourses();
+        _ = SimulateSelectedCountChanged();
+    }
+
+    private void SetSelectableCourses()
+    {
+        string content = File.ReadAllText(Path.Join(AppDomain.CurrentDomain.BaseDirectory, "Mocks", "Data", "SelectableCoursesData.json"));
+        selectableCourses = JsonSerializer.Deserialize<CourseResponse?>(content);
+    }
+
+    private void SetSelectedCourses()
+    {
+        string content = File.ReadAllText(Path.Join(AppDomain.CurrentDomain.BaseDirectory, "Mocks", "Data", "SelectedCoursesData.json"));
+        selectedCourses = JsonSerializer.Deserialize<List<CourseItem>?>(content);
+    }
+
     public Task AddCourseSelectPlan(CourseItem course, DateTime openTime, CancellationToken cancellationToken, int interval = 2000)
     {
         throw new NotImplementedException();
     }
 
-    public Task<CourseResponse?> GetSelectableCoursesAsync()
+    public async Task<CourseResponse?> GetSelectableCoursesAsync()
     {
-        throw new NotImplementedException();
+        await Task.Delay(700);
+        return selectableCourses;
     }
 
-    public Task<List<CourseItem>?> GetSelectedCoursesAsync()
+    public async Task<List<CourseItem>?> GetSelectedCoursesAsync()
     {
-        throw new NotImplementedException();
+        await Task.Delay(700);
+        return selectedCourses;
+    }
+
+    private async Task SimulateSelectedCountChanged()
+    {
+        while (true)
+        {
+            bool completed = true;
+            await Task.Delay(1500);
+            foreach (var course in selectableCourses!.Rows)
+            {
+                if (int.Parse(course.SelectedStudentCount!) < int.Parse(course.PlannedStudentCount!))
+                {
+                    int count = int.Parse(course.SelectedStudentCount!);
+                    count++;
+                    course.SelectedStudentCount = count.ToString();
+                    completed = false;
+                }
+            }
+            if (completed)
+                return;
+        }
     }
 }
