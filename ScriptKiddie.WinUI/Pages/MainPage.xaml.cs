66ba2169746d8020cc1792e871b6a30db5686cee
Zhang <Zhang747326@outlook.com>
2026-08-18 22:52:08 +0800

    调试、导航历史

--- ScriptKiddie.WinUI/Pages/MainPage.xaml.cs
+++ ScriptKiddie.WinUI/Pages/MainPage.xaml.cs
@@ -1,24 +1,30 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.UI.Xaml.Controls;
 using ScriptKiddie.WinUI.Services;
+using System.Linq;
 using WinRT;
 
 namespace ScriptKiddie.WinUI.Pages;
 
 public sealed partial class MainPage : Page
 {
-    private string currentPageTag = "home";
+    private readonly ILogger<MainPage> logger;
+    private string? currentPageTag = "home";
 
     public MainPage()
     {
         InitializeComponent();
+        logger = App.Current.Services.GetRequiredService<ILogger<MainPage>>();
         App.Current.Services.GetRequiredService<NavigationService>().Initialize(MainFrame);
         MainFrame.Navigate(typeof(HomePage));
         NavigationView.SelectedItem = NavigationView.MenuItems[0];
     }
 
-    private void navigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+    private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
     {
+        //logger.LogDebug("导航到页面：{args}", args.InvokedItemContainer.Tag.ToString());
+
         if (args.IsSettingsInvoked)
         {
             currentPageTag = "settings";
@@ -26,10 +32,13 @@ public sealed partial class MainPage : Page
         }
         else
         {
-            string tag = args.InvokedItemContainer.Tag.As<string>();
+            string? tag = args.InvokedItemContainer.Tag.ToString();
+
             if (currentPageTag == tag)
                 return;
+
             currentPageTag = tag;
+
             if (tag == "home")
             {
                 MainFrame.Navigate(typeof(HomePage));
@@ -52,6 +61,32 @@ public sealed partial class MainPage : Page
     private void NavigationView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
     {
         if (MainFrame.CanGoBack)
+        {
+            var page = MainFrame.BackStack.Last().SourcePageType;
+            //logger.LogDebug("回退到页面：{page}", page);
+
+            if (page.Equals(typeof(HomePage)))
+            {
+                currentPageTag = "home";
+                NavigationView.SelectedItem = NavigationView.MenuItems[0];
+            }
+            else if (page.Equals(typeof(AccountManagePage)))
+            {
+                currentPageTag = "accountmanage";
+                NavigationView.SelectedItem = NavigationView.MenuItems[1];
+            }
+            else if (page.Equals(typeof(CourseListPage)))
+            {
+                currentPageTag = "courselist";
+                NavigationView.SelectedItem = NavigationView.MenuItems[2];
+            }
+            else if (page.Equals(typeof(ShowacasePage)))
+            {
+                currentPageTag = "showacase";
+                NavigationView.SelectedItem = NavigationView.MenuItems[3];
+            }
+
             MainFrame.GoBack();
+        }
     }
 }
--- ScriptKiddie.WinUI/Services/NavigationService.cs
+++ ScriptKiddie.WinUI/Services/NavigationService.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media.Animation;
 
 namespace ScriptKiddie.WinUI.Services;
 
 public class NavigationService
 {
+    private readonly ILogger<NavigationService> logger = App.Current.Services.GetRequiredService<ILogger<NavigationService>>();
+
     private Frame? frame;
 
     public void Initialize(Frame frame)
@@ -14,6 +18,7 @@ public class NavigationService
 
     public void NavigateTo<T>(object? parameter = null) where T : Page
     {
+        //logger.LogDebug("导航到页面：{page}", typeof(T));
         frame?.Navigate(typeof(T), parameter, new DrillInNavigationTransitionInfo());
     }
 
